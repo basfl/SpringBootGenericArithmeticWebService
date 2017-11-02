cf67916624d2227160973e32ec5b6b76f01df371
babak soltanifar <jupitermaster83@yahoo.com>
2017-11-01 23:29:39 -0400

    add the geeric library

--- src/main/java/com/EndPoint.java
+++ src/main/java/com/EndPoint.java
@@ -7,18 +7,23 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.gen.GetAddRequest;
 import com.gen.GetAddResponse;
+import com.generic.arithmetic.Arithmetic;
+
 @Endpoint
 public class EndPoint {
 	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
 
+	Arithmetic arithmetic;
+
+	@SuppressWarnings("static-access")
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAddRequest")
 	@ResponsePayload
 	public GetAddResponse getAdd(@RequestPayload GetAddRequest request) {
 		System.out.println("*******************************************");
 		GetAddResponse response = new GetAddResponse();
-		String test = request.getBase() + request.getNumber1() + request.getNumber2();
+		String test = arithmetic.add(Integer.parseInt(request.getBase().trim().intern()), request.getNumber1(),
+				request.getNumber2());
 		response.setResult(test);
-		System.out.println("response "+response.getResult());
 		return response;
 	}
 }
